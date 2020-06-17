b075fcd59c0da210be863ae295002df82428f8dd
mathru <support@mathru.net>
2020-06-17 23:03:44 +0900

    Bug fix.

--- lib/widget/mixin/uitexteditingmixin.dart
+++ lib/widget/mixin/uitexteditingmixin.dart
@@ -13,6 +13,8 @@ abstract class UITextEditingMixin implements UIWidget {
   /// The saved value is getting by [context.consume].
   ///
   /// Override and use.
+  ///
+  /// [context]: Build context.
   @override
   @mustCallSuper
   List provider(BuildContext context) {
