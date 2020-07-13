aeeb18a98999432295a80215793ae2f847e5d5ad
mathru <user@mathru.net>
2020-07-13 19:24:42 +0900

    UITaskの更新

--- lib/widget/dialog/uitask.dart
+++ lib/widget/dialog/uitask.dart
@@ -40,29 +40,33 @@ class _UITask {
       if (task == null) {
         _title = errorTitle;
         _text = "This data is invalid.".localize();
-      } else if (task is Future<ITask>) {
-        ITask tmp = await task;
-        if (tmp == null || tmp.isTimeout) {
-          _title = errorTitle;
-          _text = timeoutText ?? tmp?.message ?? "Timed out.".localize();
-        } else if (tmp.isError) {
-          _title = errorTitle;
-          _text = tmp.message ?? Const.empty;
-        } else if (tmp.isAbort) {
-          _title = abortTitle;
-          _text = tmp.message ?? Const.empty;
+      } else if (task is Future<IPath>) {
+        IPath tmp = await task;
+        if (tmp is ITask) {
+          if (tmp.isTimeout) {
+            _title = errorTitle;
+            _text = timeoutText ?? tmp?.message ?? "Timed out.".localize();
+          } else if (tmp.isError) {
+            _title = errorTitle;
+            _text = tmp.message ?? Const.empty;
+          } else if (tmp.isAbort) {
+            _title = abortTitle;
+            _text = tmp.message ?? Const.empty;
+          }
         }
-      } else if (task is Future<List<ITask>>) {
-        List<ITask> tmp = await task;
-        if (tmp == null || tmp.isTimeout) {
-          _title = errorTitle;
-          _text = timeoutText ?? tmp?.message ?? "Timed out.".localize();
-        } else if (tmp.isError) {
-          _title = errorTitle;
-          _text = tmp.message ?? Const.empty;
-        } else if (tmp.isAbort) {
-          _title = abortTitle;
-          _text = tmp.message ?? Const.empty;
+      } else if (task is Future<List<IPath>>) {
+        List<IPath> tmp = await task;
+        if (tmp is List<ITask>) {
+          if (tmp.isTimeout) {
+            _title = errorTitle;
+            _text = timeoutText ?? tmp?.message ?? "Timed out.".localize();
+          } else if (tmp.isError) {
+            _title = errorTitle;
+            _text = tmp.message ?? Const.empty;
+          } else if (tmp.isAbort) {
+            _title = abortTitle;
+            _text = tmp.message ?? Const.empty;
+          }
         }
       }
       if (_title == null || _text == null) return;
@@ -102,8 +106,8 @@ class _UITask {
   }
 }
 
-/// Extension methods for ITask.
-extension UITaskExtension<T extends ITask> on Future<T> {
+/// Extension methods for IPath.
+extension UITaskExtension<T extends IPath> on Future<T> {
   /// Show indicator and dialog.
   ///
   /// Display an indicator if you are waiting for the task to end,
@@ -161,8 +165,67 @@ extension UITaskExtension<T extends ITask> on Future<T> {
   }
 }
 
+/// Extension methods for IPath.
+extension UITaskPathListExtension<T extends IPath> on Future<List<T>> {
+  /// Show indicator and dialog.
+  ///
+  /// Display an indicator if you are waiting for the task to end,
+  /// Display a dialog on exit on error or interruption.
+  ///
+  /// [context]: Build context.
+  /// [isShowDialog]: True to display a dialog.
+  /// [isShowIndicator]: True to display a indicator.
+  /// [dialogTitlePath]: Dialog title path.
+  /// [dialogTextPath]: Dialog text path.
+  /// [dialogSubmitTextPath]: Dialog submit button text path.
+  /// [dialogSubmitActionPath]: The path of action when the submit button of the dialog is pressed.
+  /// [submitText]: Default submit button text.
+  /// [submitAction]: Default submit button action.
+  /// [title]: Default title.
+  /// [text]: Default text.
+  /// [errorTitle]: Title at the end of error.
+  /// [abortTitle]: Title at the end of the break.
+  /// [timeoutText]: Message when timed out.
+  Future<List<T>> showIndicatorAndDialog(BuildContext context,
+      {bool isShowDialog = true,
+      bool isShowIndicator = true,
+      String dialogTitlePath = DefaultPath.dialogTitle,
+      String dialogTextPath = DefaultPath.dialogText,
+      String dialogSubmitTextPath = DefaultPath.dialogSubmitText,
+      String dialogSubmitActionPath = DefaultPath.dialogSubmitAction,
+      String submitText = "OK",
+      String title,
+      String text,
+      VoidAction submitAction,
+      String errorTitle = "ERROR",
+      String abortTitle = "ABORTED",
+      String timeoutText}) async {
+    Future.delayed(
+        Duration.zero,
+        () => _UITask.show(context, this,
+            isShowDialog: isShowDialog,
+            isShowIndicator: isShowIndicator,
+            dialogTitlePath: dialogTitlePath,
+            dialogTextPath: dialogTextPath,
+            dialogSubmitTextPath: dialogSubmitTextPath,
+            dialogSubmitActionPath: dialogSubmitActionPath,
+            title: title,
+            text: text,
+            submitAction: submitAction,
+            submitText: submitText,
+            errorTitle: errorTitle,
+            abortTitle: abortTitle,
+            timeoutText: timeoutText));
+    return this.then((value) async {
+      await Future.delayed(Duration.zero);
+      await Future.delayed(Duration.zero);
+      return value;
+    });
+  }
+}
+
 /// Extension methods for ITask.
-extension UITaskListExtension<T extends ITask> on Future<List<T>> {
+extension UITaskTaskListExtension<T extends ITask> on Future<List<T>> {
   /// Show indicator and dialog.
   ///
   /// Display an indicator if you are waiting for the task to end,
