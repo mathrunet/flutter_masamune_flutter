28d1073f5360cdd4d3d31e958dea62234b7ed397
mathru <user@mathru.net>
2020-09-25 14:52:45 +0900

    Model系を削除（flutter_widget_modelに移動）

--- lib/masamune_flutter.dart
+++ lib/masamune_flutter.dart
@@ -49,7 +49,3 @@ part 'route/uirouteobserver.dart';
 
 part 'animation/uianimatorscenario.dart';
 part 'animation/uianimatorunit.dart';
-
-part 'model/unitmodel.dart';
-part 'model/documentmodel.dart';
-part 'model/collectionmodel.dart';
